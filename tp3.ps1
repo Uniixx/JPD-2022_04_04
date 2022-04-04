ab2c2243c21d35ebc3c5220ec4e174077b33b4cf
Uniixx <nathanvez2@gmail.com>
2022-04-04 10:24:58 -0400

    Création d'un utilisateur

--- tp3.ps1
+++ tp3.ps1
@@ -1,1 +1,14 @@
-ï»¿
+Clear-Host
+
+function CreateUser($name) {
+  $user = Get-LocalUser | Where-Object Name -eq $name;
+
+  if (!$user) {
+    $user = New-LocalUser -Name $name -FullName $name -Description "Compte pour $name";
+    Write-Host "Création de l'utilisateur $name avec succès!";
+  } else {
+    Write-Host "Utilisateur déja éxistant!";
+  }
+}
+
+CreateUser("nathan");
